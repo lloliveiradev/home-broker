3a4f7c21ddbc7f2bfd0ad34c3bd085defbcc0e02
Leonardo Oliveira <leonardo@apoiotech.com.br>
2025-02-18 12:50:03 -0300

    2025-02-17 - building backend in nest, mongodb and docker

---
+++ backend/src/wallets/wallets.controller.ts
@@ -0,0 +1,44 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { WalletsService } from './wallets.service';
+import { CreateWalletDto } from './dto/create-wallet.dto';
+import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { CreateWalletAssetDto } from './dto/create-wallet-asset.dto';
+
+@Controller('wallets')
+export class WalletsController {
+  constructor(private readonly walletsService: WalletsService) { }
+
+  @Post()
+  create(@Body() createWalletDto: CreateWalletDto) {
+    return this.walletsService.create(createWalletDto);
+  }
+
+  @Post(':id/assets')
+  createWalletAsset(@Param('id') id: string, @Body() body: CreateWalletAssetDto) {
+    return this.walletsService.createWalletAsset({
+      walletId: id,
+      assetId: body.assetId,
+      shares: body.shares
+    });
+  }
+
+  @Get()
+  findAll() {
+    return this.walletsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.walletsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateWalletDto: UpdateWalletDto) {
+    return this.walletsService.update(id, updateWalletDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.walletsService.remove(id);
+  }
+}
---
+++ backend/src/wallets/wallets.service.ts
@@ -0,0 +1,73 @@
+import { Injectable } from '@nestjs/common';
+import { CreateWalletDto } from './dto/create-wallet.dto';
+import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Wallet } from './entities/wallet.entity';
+import mongoose, { Model } from 'mongoose';
+import { WalletAsset } from './entities/wallet-asset.entity';
+
+@Injectable()
+export class WalletsService {
+  constructor(
+    @InjectModel(Wallet.name) private walletSchema: Model<Wallet>,
+    @InjectModel(WalletAsset.name) private walletAssetSchema: Model<WalletAsset>,
+    @InjectConnection() private connection: mongoose.Connection
+  ) { }
+
+  create(createWalletDto: CreateWalletDto) {
+    return this.walletSchema.create(createWalletDto);
+  }
+
+  findAll() {
+    return this.walletSchema.find();
+  }
+
+  findOne(id: string) {
+    return this.walletSchema.findById(id).populate([
+      {
+        path: 'assets',
+        populate: ['asset']
+      }
+    ]);
+  }
+
+  update(id: string, updateWalletDto: UpdateWalletDto) {
+    return this.walletSchema.updateOne({ _id: id }, updateWalletDto);
+  }
+
+  remove(id: string) {
+    return this.walletSchema.deleteOne({ _id: id });
+  }
+
+  async createWalletAsset(data: { walletId: string, assetId: string, shares: number }) {
+    const session = await this.connection.startSession();
+    try {
+      await session.startTransaction();
+
+      const docs = await this.walletAssetSchema.create(
+        [{
+          wallet: data.walletId,
+          asset: data.assetId,
+          shares: data.shares
+        }],
+        { session }
+      );
+      const walletAsset = docs[0];
+
+      this.walletAssetSchema.updateOne(
+        { _id: data.walletId },
+        { $push: { assets: walletAsset._id } },
+        { session }
+      );
+
+      await session.commitTransaction();
+      return walletAsset;
+    } catch (error) {
+      console.error(error);
+      await session.abortTransaction();
+      throw error;
+    } finally {
+      await session.endSession();
+    };
+  }
+}
